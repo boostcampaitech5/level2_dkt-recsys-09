b9157cf8516669b3aa6decb30cafca0c0d62755e
asdftyui <bbb6254177@gmail.com>
2023-05-17 17:30:34 +0000

    #9 feat: add loss function for UltraGCN

--- DKT/model/loss.py
+++ DKT/model/loss.py
@@ -1,5 +1,42 @@
 import torch.nn.functional as F
+import torch
 
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
+
+
+def get_betas(model, users, items):
+    user_degree = model.constraint_mat['user_degree'].to('cuda')
+    item_degree = model.constraint_mat['item_degree'].to('cuda')
+    
+    weights = 1 + model.lambda_ * (1/user_degree[users]) * torch.sqrt((user_degree[users]+1)/(item_degree[items]+1))
+    
+    return weights
+    
+    
+def cal_loss_L(beta_weight, output, target):
+    
+    loss = F.binary_cross_entropy(output, target.float(), weight=beta_weight, reduction='none')
+    
+    return loss.sum()
+
+
+def norm_loss(model):
+    loss = 0.0
+    for parameter in model.parameters():
+        loss += torch.sum(parameter ** 2)
+    return loss / 2
+
+
+def UltraGCN_loss(model, output, data, target):
+    
+    users = data[:, 0]
+    items = data[:, 1]
+    
+    beta_weight = get_betas(model, users, items)
+    
+    loss = cal_loss_L(beta_weight, output, target) 
+    loss += model.gamma * norm_loss(model)
+
+    return loss
