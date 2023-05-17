d6bfe127043bc0d8794b9ca455cd45b9b123da81
asdftyui <bbb6254177@gmail.com>
2023-05-17 17:28:39 +0000

    #9 feat: add UltraGCN model

--- DKT/model/model.py
+++ DKT/model/model.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
-import torch
 import pickle
 
 class MnistModel(BaseModel):
@@ -26,15 +25,11 @@ class MnistModel(BaseModel):
 class UltraGCN(nn.Module):
     def __init__(self, **params):
         super(UltraGCN, self).__init__()
+        
         self.user_num = params['user_num']
         self.item_num = params['item_num']
         self.embedding_dim = params['embedding_dim']
-        self.w1 = params['w1']
-        self.w2 = params['w2']
-        self.w3 = params['w3']
-        self.w4 = params['w4']
-
-        self.negative_weight = params['negative_weight']
+        
         self.gamma = params['gamma']
         self.lambda_ = params['lambda']
 
@@ -44,83 +39,18 @@ class UltraGCN(nn.Module):
         with open('constraint_matrix.pickle', 'rb') as f:
             self.constraint_mat = pickle.load(f)
 
-        self.initial_weight = params['initial_weight']
         self.initial_weights()
 
     def initial_weights(self):
-        nn.init.normal_(self.user_embeds.weight, std=self.initial_weight)
-        nn.init.normal_(self.item_embeds.weight, std=self.initial_weight)
+        nn.init.xavier_normal_(self.user_embeds.weight)
+        nn.init.xavier_normal_(self.item_embeds.weight)
 
-    def get_omegas(self, users, pos_items, neg_items):
-        device = self.get_device()
-        if self.w2 > 0:
-            pos_weight = torch.mul(self.constraint_mat['user_degree'][users], self.constraint_mat['item_degree'][pos_items]).to(device)
-            pow_weight = self.w1 + self.w2 * pos_weight
-        else:
-            pos_weight = self.w1 * torch.ones(len(pos_items)).to(device)
+    def forward(self, data):
         
-        # users = (users * self.item_num).unsqueeze(0)
-        if self.w4 > 0:
-            neg_weight = torch.mul(torch.repeat_interleave(self.constraint_mat['user_degree'][users], neg_items.size(1)), self.constraint_mat['item_degree'][neg_items.flatten()]).to(device)
-            neg_weight = self.w3 + self.w4 * neg_weight
-        else:
-            neg_weight = self.w3 * torch.ones(neg_items.size(0) * neg_items.size(1)).to(device)
-
-
-        weight = torch.cat((pow_weight, neg_weight))
-        return weight
-
-    def cal_loss_L(self, users, pos_items, neg_items, omega_weight):
-        device = self.get_device()
-        user_embeds = self.user_embeds(users)
-        pos_embeds = self.item_embeds(pos_items)
-        neg_embeds = self.item_embeds(neg_items)
-      
-        pos_scores = (user_embeds * pos_embeds).sum(dim=-1) # batch_size
-        user_embeds = user_embeds.unsqueeze(1)
-        neg_scores = (user_embeds * neg_embeds).sum(dim=-1) # batch_size * negative_num
-
-        neg_labels = torch.zeros(neg_scores.size()).to(device)
-        neg_loss = F.binary_cross_entropy_with_logits(neg_scores, neg_labels, weight = omega_weight[len(pos_scores):].view(neg_scores.size()), reduction='none').mean(dim = -1)
+        users = data[:, 0]
+        items = data[:, 1]
         
-        pos_labels = torch.ones(pos_scores.size()).to(device)
-        pos_loss = F.binary_cross_entropy_with_logits(pos_scores, pos_labels, weight = omega_weight[:len(pos_scores)], reduction='none')
-
-        loss = pos_loss + neg_loss * self.negative_weight
-      
-        return loss.sum()
-
-    # def cal_loss_I(self, users, pos_items):
-    #     device = self.get_device()
-    #     neighbor_embeds = self.item_embeds(self.ii_neighbor_mat[pos_items].to(device))    # len(pos_items) * num_neighbors * dim
-    #     sim_scores = self.ii_constraint_mat[pos_items].to(device)     # len(pos_items) * num_neighbors
-    #     user_embeds = self.user_embeds(users).unsqueeze(1)
-        
-    #     loss = -sim_scores * (user_embeds * neighbor_embeds).sum(dim=-1).sigmoid().log()
-      
-    #     # loss = loss.sum(-1)
-    #     return loss.sum()
-
-    def norm_loss(self):
-        loss = 0.0
-        for parameter in self.parameters():
-            loss += torch.sum(parameter ** 2)
-        return loss / 2
-
-    def forward(self, users, pos_items, neg_items):
-        omega_weight = self.get_omegas(users, pos_items, neg_items)
-        
-        loss = self.cal_loss_L(users, pos_items, neg_items, omega_weight)
-        loss += self.gamma * self.norm_loss()
-        # loss += self.lambda_ * self.cal_loss_I(users, pos_items)
-        return loss
-
-    def test_foward(self, users):
-        items = torch.arange(self.item_num).to(users.device)
         user_embeds = self.user_embeds(users)
         item_embeds = self.item_embeds(items)
          
-        return user_embeds.mm(item_embeds.t())
-
-    def get_device(self):
-        return self.user_embeds.weight.device
+        return (user_embeds * item_embeds).sum(dim=-1).sigmoid()
