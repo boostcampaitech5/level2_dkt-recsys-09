8073d1e2d6e07de78bc59ed517c70df57e33f217
asdftyui <bbb6254177@gmail.com>
2023-05-17 17:32:15 +0000

    #9 fix: fix accuary function for UltraGCN

--- DKT/model/metric.py
+++ DKT/model/metric.py
@@ -3,7 +3,7 @@ import torch
 
 def accuracy(output, target):
     with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
+        pred = output.round()
         assert pred.shape[0] == len(target)
         correct = 0
         correct += torch.sum(pred == target).item()
