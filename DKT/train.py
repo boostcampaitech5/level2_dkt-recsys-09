df02f409bf02dcad70d433926b70c7e19264b6b7
asdftyui <bbb6254177@gmail.com>
2023-05-18 03:42:58 +0000

    #9 feat: add code for wandb

--- DKT/train.py
+++ DKT/train.py
@@ -9,6 +9,7 @@ import model.model as module_arch
 from parse_config import ConfigParser
 from trainer import Trainer
 from utils import prepare_device
+import wandb
 
 
 # fix random seeds for reproducibility
@@ -19,12 +20,14 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(SEED)
 
 def main(config):
+    wandb.login()
     logger = config.get_logger('train')
 
     # setup data_loader instances
     data_loader = config.init_obj('data_loader', module_data)
     valid_data_loader = data_loader.split_validation()
 
+    wandb.init(project=config['name'], config=config, entity="ffm")
     # build model architecture, then print to console
     model = config.init_obj('arch', module_arch)
     logger.info(model)
