ec416b0d969d9d6707c1ae1afb0a30a1c7537f28
asdftyui <bbb6254177@gmail.com>
2023-05-22 04:07:52 +0000

    #17 feat: add preprocessing code for hybrid model

--- DKT/data_loader/data_preprocess_GCN.py
+++ DKT/data_loader/data_preprocess_GCN.py
@@ -3,6 +3,10 @@ import time
 import datetime
 import pickle
 import torch
+import os
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
 
 def ultragcn_preprocess(train, test):
     
@@ -47,3 +51,49 @@ def save_constraint_matrix(data):
     
     with open('constraint_matrix.pickle', 'wb') as f:
         pickle.dump(constraint_mat, f)
+        
+
+def hybrid_preprocess(data_dir, args):
+    df = pd.read_csv(os.path.join(data_dir, "train_data.csv"))
+    df = __preprocessing(df)
+
+    # 추후 feature를 embedding할 시에 embedding_layer의 input 크기를 결정할때 사용
+    args.n_questions = df['assessmentItemID'].nunique()
+    args.n_test = df['testId'].nunique()
+    args.n_tag = df['KnowledgeTag'].nunique()
+
+    df = df.sort_values(by=['userID','Timestamp'], axis=0)
+    columns = ['userID', 'assessmentItemID', 'testId', 'answerCode', 'KnowledgeTag']
+    group = df[columns].groupby('userID').apply(
+            lambda r: (
+                r['testId'].values, 
+                r['assessmentItemID'].values,
+                r['KnowledgeTag'].values,
+                r['answerCode'].values
+            )
+        )
+
+def __save_labels(encoder, name, args):
+        le_path = os.path.join(args.data_dir, name + '_classes.npy')
+        np.save(le_path, encoder.classes_)
+
+
+def __preprocessing(df, args):
+    cate_cols = ['assessmentItemID', 'testId', 'KnowledgeTag']
+    for col in cate_cols:
+
+        #For UNKNOWN class
+        a = df[col].unique().tolist() + [np.nan]
+        
+        le = LabelEncoder()
+        le.fit(a)
+        df[col] = le.transform(df[col])
+        __save_labels(le, col, args)
+
+    def convert_time(s):
+        timestamp = time.mktime(datetime.strptime(s, '%Y-%m-%d %H:%M:%S').timetuple())
+        return int(timestamp)
+
+    df['Timestamp'] = df['Timestamp'].apply(convert_time)
+    
+    return df
