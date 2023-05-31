981eb18ed35a75b213f5bfb7332532244ec07931
HeewonKwak <hewo1217@gmail.com>
2023-05-31 04:01:46 +0000

    refactor: kfold 적용 추가 - config 형식 변경 - kfold test 적용 #32

--- DKT/test_GCN.py
+++ DKT/test_GCN.py
@@ -3,6 +3,8 @@ import torch
 import model.model_GCN as module_arch
 from parse_config import ConfigParser
 import pandas as pd
+import numpy as np
+import os
 from torch.utils.data import DataLoader, TensorDataset
 
 
@@ -11,15 +13,34 @@ def main(config):
     test_dataset = TensorDataset(torch.LongTensor(data.values))
     test_dataloader = DataLoader(test_dataset, batch_size=config['test']['batch_size'],  shuffle=False)
 
-    # build model architecture
-    model = config.init_obj('arch', module_arch)
-    model.load_state_dict(torch.load(config['test']['model_dir'])['state_dict'])
-    model.eval()
+    if config['fold']:
+        predicts_list = list()
+        for fold in range(5):
+            # build model architecture
+            model = config.init_obj('arch', module_arch)
+            model_path = config['test']['model_dir']+"{}.pth".format(fold)
+            model.load_state_dict(torch.load(model_path)['state_dict'])
+            model.eval()
+            
+            predict = list()
+            for idx, data in enumerate(test_dataloader):
+                predict.extend(model(data[0]).tolist())
+            predicts_list.append(predict)
+        predicts = np.mean(predicts_list, axis=0)
+    else:
+        # build model architecture
+        model = config.init_obj('arch', module_arch)
+        model_path = config['test']['model_dir']+"0.pth"
+        model.load_state_dict(torch.load(model_path)['state_dict'])
+        model.eval()
     
-    predicts = list()
-    for idx, data in enumerate(test_dataloader):
-        predicts.extend(model(data[0]).tolist())
+        predicts = list()
+        for idx, data in enumerate(test_dataloader):
+            predicts.extend(model(data[0]).tolist())
         
+    dir_path = "./submission"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
     write_path = config['test']['submission_dir']
     submission = pd.read_csv(config['test']['sample_submission_dir'])
     submission['prediction'] = predicts
